ae6d525e71be18eec5844bb12799218ff25c6f2b
itschool <itschool@tou.edu.kz>
2026-02-15 10:45:41 +0500

    15febr

--- mysql.fe/src/pages/HomePage.vue
+++ mysql.fe/src/pages/HomePage.vue
@@ -4,12 +4,14 @@
         data() {
             return {
                 title: '',
+                users: []
             }
         },
         methods: {
             async loadTitle() {
                 const result = await axios.get('http://mysql.be/index.php')
                 this.title = result.data.title
+                this.users = result.data.users
             }
         }
     }
@@ -20,6 +22,35 @@
   <button
     @click="loadTitle"
   >Загрузить данные</button>
+<table>
+    <tr>
+        <td>ID</td>
+        <td>Имя</td>
+        <td>Фамилия</td>
+        <td>Возраст</td>
+    </tr>
+    <tr
+        v-for="user in users"
+        :key="user.id"
+    >
+        <td>{{ user.id }}</td>
+        <td>{{ user.firstname }}</td>
+        <td>{{ user.secondname }}</td>
+        <td>{{ user.age }}</td>
+    </tr>
+</table>
 </template>
 
-<style scoped></style>
+<style>
+    table, td {
+        border: 1px solid black;
+    }
+
+    table {
+        border-collapse: collapse;
+    }
+
+    td {
+        padding: 5px 10px;
+    }
+</style>
